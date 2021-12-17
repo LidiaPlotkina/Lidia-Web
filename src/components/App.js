50d03da6459dc33d0eb2f98acf9fbddbc6978008
Gaurav <gaurav.kharge@ibronx.com>
2021-12-17 09:30:31 +0800

    Add: New Updates added

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Navbar transparent={true} />
       <main>
-        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen" id="home">
+        <div className="relative flex content-center items-center justify-center min-h-screen" id="home">
           <BackGroundCover />
         </div>
         <AboutMe />
